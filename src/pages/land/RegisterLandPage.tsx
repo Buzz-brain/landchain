ab1c41616da5c78de8a15a144c7e2478a503aaeb
Buzz-brain <chinomsochristian03@gmail.com>
2025-09-02 11:20:56 +0100

    Minor updates

--- src/pages/land/RegisterLandPage.tsx
+++ src/pages/land/RegisterLandPage.tsx
@@ -105,7 +105,7 @@ export function RegisterLandPage() {
 
               <Input
                 label="Property Size"
-                type="number"
+                type="text"
                 step="0.1"
                 value={formData.size}
                 onChange={(e) =>
